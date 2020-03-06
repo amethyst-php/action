8d5c309f56362dbba69e8b6596791e0d8718dc16
Railken <railken@protonmail.ch>
2020-03-06 20:28:42 +0100

    fix serialize/unserialize volatile models

--- src/Services/Bag.php
+++ src/Services/Bag.php
@@ -4,6 +4,10 @@ namespace Amethyst\Services;
 
 use Illuminate\Queue\SerializesModels;
 use Railken\Bag as BaseBag;
+use Illuminate\Contracts\Queue\QueueableEntity;
+use Illuminate\Contracts\Database\ModelIdentifier;
+use Illuminate\Contracts\Queue\QueueableCollection;
+use Railken\EloquentMapper\Contracts\Map as MapContract;
 
 class Bag extends BaseBag
 {
@@ -37,5 +41,47 @@ class Bag extends BaseBag
         foreach ($properties as $name => $property) {
             $this->set($name, $this->getRestoredPropertyValue($property));
         }
+    }
+
+    /**
+     * Get the property value prepared for serialization.
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    protected function getSerializedPropertyValue($value)
+    {
+        if ($value instanceof QueueableCollection) {
+            return new ModelIdentifier(
+                $value->getQueueableClass(),
+                $value->getQueueableIds(),
+                $value->getQueueableRelations(),
+                $value->getQueueableConnection()
+            );
+        }
+
+        if ($value instanceof QueueableEntity) {
+            return new ModelIdentifier(
+                app(MapContract::class)->modelToKey($value),
+                $value->getQueueableId(),
+                $value->getQueueableRelations(),
+                $value->getQueueableConnection()
+            );
+        }
+
+        return $value;
+    }
+
+    /**
+     * Restore the model from the model identifier instance.
+     *
+     * @param  \Illuminate\Contracts\Database\ModelIdentifier  $value
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function restoreModel($value)
+    {
+        return $this->getQueryForModelRestoration(
+            app(MapContract::class)->keyToModel($value->class)->setConnection($value->connection), $value->id
+        )->useWritePdo()->firstOrFail()->load($value->relations ?? []);
     }
 }
